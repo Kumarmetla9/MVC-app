6b9e2d18330e82e602a560a4a1fc335904555855
Kumarmetla9 <kumarmetla9@gmail.com>
2016-10-10 14:06:53 -0400

    Add files via upload

---
+++ DemoInformationService/DemoInformationService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+using DataAccess;
+
+namespace DemoInformationService
+{
+    public class DemoInformationService : IDemoInformationService
+    {
+        public DataSet GetDemographicsWithOutDB()
+        {
+            return DADemographicsInformation.GetDemographicsWithOutDB();
+        }
+
+        public DataSet GetDemographicsUsingDBWithConfig()
+        {
+            return DADemographicsInformation.GetDemographicsUsingDBWithConfig();
+        }
+
+        public DataSet GetDemographicsUsingDBWithOutConfig()
+        {
+            return DADemographicsInformation.GetDemographicsUsingDBWithOutConfig();
+        }
+    }
+}
---
+++ DemoInformationService/IDemoInformationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+
+namespace DemoInformationService
+{
+    [ServiceContract]
+    public interface IDemoInformationService
+    {
+        [OperationContract]
+        DataSet GetDemographicsWithOutDB();
+
+        [OperationContract]
+        DataSet GetDemographicsUsingDBWithConfig();
+
+        [OperationContract]
+        DataSet GetDemographicsUsingDBWithOutConfig();
+    }
+}
